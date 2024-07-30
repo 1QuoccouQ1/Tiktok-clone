52f61a0067006ccbb59465f33d7fc191190cedd3
1QuoccouQ1 <bqgaming2004@gmail.com>
2024-07-30 17:19:47 +0700

    first commit

---
+++ src/components/SuggestedAccounts/AccountPreview/AccountPreview.js
@@ -0,0 +1,40 @@
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+import Button from '~/components/Button';
+import styles from './AccountPreview.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountPreview() {
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('header')}>
+                <img
+                    className={cx('avatar')}
+                    src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1qQzbt.img?w=768&h=512&m=6&x=123&y=96&s=587&d=65"
+                    alt=""
+                />
+                <Button className={cx('follow-btn')} primary>
+                    Follow
+                </Button>
+            </div>
+            <div className={cx('body')}>
+                <p className={cx('nickname')}>
+                    <strong>quocnguyenphu</strong>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </p>
+                <p className={cx('name')}>Quốc Nguyễn Phú</p>
+                <p className={cx('analytics')}>
+                    <strong className={cx('value')}>8.2M </strong>
+                    <span className={cx('label')}>Followers</span>
+                    <strong className={cx('value')}>8.2M </strong>
+                    <span className={cx('label')}>Likes</span>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default AccountPreview;
